4e523a9e8573f5294511adf44bfbceea688ebb4f
Zafkiel45 <112777858+Zafkiel45@users.noreply.github.com>
2023-02-22 23:48:09 -0300

    Dark blue progress

---
+++ Header/Dark Blue/js/script.js
@@ -0,0 +1,17 @@
+const svg_1 = document.querySelector('#svg');
+const sidebar = document.querySelector('#sidebar');
+const svg_2 = document.querySelector('#svg_2')
+const content = document.querySelector('#content')
+
+svg_1.addEventListener('click', function() {
+    sidebar.style.left = '0px'
+    sidebar.style.animationName = 'side'
+    content.style.filter = 'blur(5px)'
+})
+
+svg_2.addEventListener('click', function() {
+    sidebar.style.left = '-100%'
+    sidebar.style.animationName = ''
+    content.style.filter = ''
+    sidebar.style.animationName = 'invertside'
+})
